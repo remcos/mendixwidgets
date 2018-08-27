77cca943dacc52d92311d73dda7e345bc250f16a
Conner Charlebois <cdcharlebois@gmail.com>
2018-08-27 18:30:16 -0400

    removing ignored files

--- ShowByCondition/test/Test.mpr/theme/config.rb
+++
@@ -1,8 +0,0 @@
-preferred_syntax = :scss
-http_path = '/'
-css_dir = 'styles/css'
-sass_dir = 'styles/sass'
-relative_assets = true
-line_comments = false
-sourcemap = false
-output_style = :expanded
